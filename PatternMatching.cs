091e2aef65327129186aa51adc585c48b3fbe07a
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-09-16 11:51:52 +0530

    [sanjeeva] : checked for valid first name

---
+++ PatternMatching.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexExplanation
+{
+    internal class PatternMatching
+    {
+        public static void validate_first_name() 
+        {
+            Console.WriteLine("enter first name");
+            string first_name=Console.ReadLine();
+
+            string namepattern = "^[A-Z]{1}[a-zA-Z]{2,}$";
+            Regex regobj=new Regex(namepattern);
+
+            if (regobj.IsMatch(first_name))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("first name is valid");
+                Console.ResetColor();
+                
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("first name is not valid");
+                Console.ResetColor();
+            }
+        }
+        
+        
+
+        
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace RegexExplanation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("select option for validating : \n" +
+                "1.to check first name \n" +
+                "2.to check last name\n");
+
+            Console.WriteLine("enter the option");
+            int option=Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    PatternMatching.validate_first_name();
+                    break;
+                
+                default:
+                    Console.WriteLine("enter valid input");
+                    break;
+            }
+
+            
+            
+
+        }
+    }
+}
