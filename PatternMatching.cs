c4b72f1477910aae66cbbff5bbcc9b7daddda5a4
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-09-16 12:28:57 +0530

    [sanjeeva] : email validated

--- PatternMatching.cs
+++ PatternMatching.cs
@@ -7,8 +7,31 @@ using System.Text.RegularExpressions;
 
 namespace RegexExplanation
 {
+
     internal class PatternMatching
     {
+        public static void validate_first_name()
+        {
+            Console.WriteLine("enter first name");
+            string first_name = Console.ReadLine();
+
+            string namepattern = "^[A-Z]{1}[a-zA-Z]{2,}$";
+            Regex regobj = new Regex(namepattern);
+
+            if (regobj.IsMatch(first_name))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("first name is valid");
+                Console.ResetColor();
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("first name is not valid");
+                Console.ResetColor();
+            }
+        }
         public static void validate_last_name()
         {
             Console.WriteLine("enter last name");
@@ -31,7 +54,28 @@ namespace RegexExplanation
                 Console.ResetColor();
             }
         }
+        public static void validate_email()
+        {
+            Console.WriteLine("enter email");
+            string email = Console.ReadLine();
 
+            string namepattern = "^[a-z]{1,}([-.+]{1}){0,1}([0-9]{1,}){0,1}[@]{1}[a-z0-9]{1,}[.][a-zA-Z]{2,3}([.]{1}[a-z]{2,3}){0,1}$";
+            Regex regobj = new Regex(namepattern);
+
+            if (regobj.IsMatch(email))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("email is valid");
+                Console.ResetColor();
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("email is not valid");
+                Console.ResetColor();
+            }
+        }
 
 
 
--- Program.cs
+++ Program.cs
@@ -1,30 +1,34 @@
-﻿namespace RegexExplanation
+﻿using System.Text.RegularExpressions;
+
+namespace RegexExplanation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("select option for validating : \n" +
-                "1.to check first name \n" +
-                "2.to check last name\n");
+                    "1.to check first name \n" +
+                    "2.to check last name\n");
 
             Console.WriteLine("enter the option");
-            int option=Convert.ToInt32(Console.ReadLine());
+            int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
             {
+                case 1:
+                    PatternMatching.validate_first_name();
+                    break;
                 case 2:
                     PatternMatching.validate_last_name();
                     break;
-                
+                case 3:
+                    PatternMatching.validate_email();
+                    break;
                 default:
                     Console.WriteLine("enter valid input");
                     break;
+
             }
-
-            
-            
-
         }
     }
 }
